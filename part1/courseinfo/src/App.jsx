013ff93267d4c05e7657c28d30b6a0ed48e76c29
hadi <hadishamsi854@gmail.com>
2026-02-18 07:31:36 +0500

    Exercise-1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,31 +1,57 @@
+//header component
 const Header = (prop) => {
   return (
-    <>
-      <div>
-        <h1>{prop.course}</h1>
-      </div>
-    </>
+    <div>
+      <h1>{prop.course}</h1>
+    </div>
   );
 };
 
+//part1 component
+const Part1 = (prop) => {
+  return (
+    <div>
+      <p>
+        {prop.part1} {prop.exercises1}
+      </p>
+    </div>
+  );
+};
+
+//part2 component
+const Part2 = (prop) => {
+  return (
+    <div>
+      <p>
+        {prop.part2} {prop.exercises2}
+      </p>
+    </div>
+  );
+};
+
+//part3 component
+const Part3 = (prop) => {
+  return (
+    <div>
+      <p>
+        {prop.part3} {prop.exercises3}
+      </p>
+    </div>
+  );
+};
+
+//content component
 const Content = (prop) => {
   return (
-    <>
-      <div>
-        <p>
-          {prop.part1} {prop.exercises1}
-        </p>
-        <p>
-          {prop.part2} {prop.exercises2}
-        </p>
-        <p>
-          {prop.part3} {prop.exercises3}
-        </p>
-      </div>
-    </>
+    <div>
+      <Part1 part1={prop.part1} exercises1={prop.exercises1} />
+      <Part2 part2={prop.part2} exercises2={prop.exercises2} />
+      <Part3 part3={prop.part3} exercises3={prop.exercises3} />
+    </div>
   );
 };
 
+//total component
 const Total = (prop) => {
   return (
     <p>
@@ -34,6 +60,7 @@ const Total = (prop) => {
   );
 };
 
+//app component
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
