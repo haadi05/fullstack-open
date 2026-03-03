54ddc814575b9005761a2597b28636d0ddad91f7
hadi <hadishamsi854@gmail.com>
2026-03-03 14:03:37 +0500

    Exercise-2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -28,9 +28,11 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(personObj));
-      setNewName("");
-      setNewNumber("");
+      axios.post("http://localhost:3001/persons", personObj).then((res) => {
+        setPersons(persons.concat(res.data));
+        setNewName("");
+        setNewNumber("");
+      });
     }
   };
 
