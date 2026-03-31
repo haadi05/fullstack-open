465f17ae38faa6b02c70b9560c8494fbf082e16a
hadi <hadishamsi854@gmail.com>
2026-03-31 18:02:32 +0500

    Exercise-3.20

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -21,7 +21,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{2}-\d{7}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
