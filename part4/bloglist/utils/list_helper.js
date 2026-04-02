735d8a092394fdea28db7d83462efa2f6901f2f2
hadi <hadishamsi854@gmail.com>
2026-04-02 12:59:31 +0500

    Exercise-4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes);
+  const reducer = (sum, item) => (sum = sum + item);
+  return likesArray.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
