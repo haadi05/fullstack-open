1e7edc8fd0efeb77ef9d4fefb010860c38ee5cc1
hadi <hadishamsi854@gmail.com>
2026-04-03 18:05:37 +0500

    Exercise-4.6

--- part4/bloglist/tests/mostBlogs.test.js
+++ part4/bloglist/tests/mostBlogs.test.js
@@ -2,7 +2,7 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
-describe("favorite blog", () => {
+describe("most blogs", () => {
   const blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -21,11 +21,27 @@ describe("favorite blog", () => {
       __v: 0,
     },
     {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+    {
       _id: "5a422ba71b54a676234d17fb",
       title: "TDD harms architecture",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 12,
+      likes: 0,
       __v: 0,
     },
     {
@@ -33,20 +49,16 @@ describe("favorite blog", () => {
       title: "Type wars",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 12,
+      likes: 2,
       __v: 0,
     },
   ];
 
-  test("favoriteBlog returns the most liked blog", () => {
-    const result = listHelper.favoriteBlog(blogs);
+  test("mostBlogs returns the author with the highest blog count", () => {
+    const result = listHelper.mostBlogs(blogs);
     assert.deepStrictEqual(result, {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
       author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 12,
-      __v: 0,
+      blogs: 3,
     });
   });
 });
