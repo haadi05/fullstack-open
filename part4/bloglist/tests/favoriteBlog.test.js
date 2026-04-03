8417440e94ce444650b8d899911a06bbbf8a9ad2
hadi <hadishamsi854@gmail.com>
2026-04-03 06:52:10 +0500

    Exercise-4.5

---
+++ part4/bloglist/tests/favoriteBlog.test.js
@@ -0,0 +1,51 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
+
+describe("favorite Blog", () => {
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 12,
+      __v: 0,
+    },
+  ];
+  test("favorite blog of list is the most liked one", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (blogs) => {
   return likesArray.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes);
+
+  const reducer = (max, item) => {
+    if (item > max) {
+      max = item;
+    }
+    return max;
+  };
+
+  const mostLikedCount = likesArray.reduce(reducer, 0);
+  const favBlogsList = blogs.filter((blog) => blog.likes === mostLikedCount);
+  return favBlogsList[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
