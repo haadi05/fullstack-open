ce374b9c2a438a77871965082b08f7826e5240ef
hadi <hadishamsi854@gmail.com>
2026-04-05 07:27:32 +0500

    Exercise-4.9

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -18,6 +18,11 @@ test("bloglist returned correct amount of blog posts", async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
+test("unique identifier property of blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+  assert.strictEqual("id" in response.body[0], true);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
