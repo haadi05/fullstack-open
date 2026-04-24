ea5078f1f7acf6957208e5dd72c9ab58dac2410f
hadi <hadishamsi854@gmail.com>
2026-04-25 00:15:00 +0500

    Exercise-5.7

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,9 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-    <br />
-    {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [toggleFullView, setToggleFullView] = useState(false);
+  return (
+    <div style={{ margin: "8px", padding: "4px", border: "1px solid black" }}>
+      {toggleFullView ? (
+        <div>
+          {blog.title}{" "}
+          <button onClick={() => setToggleFullView(false)}>hide</button>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      ) : (
+        <div>
+          {blog.title}{" "}
+          <button onClick={() => setToggleFullView(true)}>show</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
