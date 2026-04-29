c6358ba6d62933cd73a4aec6bbc0399d7ff78e4e
hadi <hadishamsi854@gmail.com>
2026-04-30 01:51:51 +0500

    Exercise-5.14

--- part5/bloglist_frontend/src/tests/Bloglist.test.jsx
+++ part5/bloglist_frontend/src/tests/Bloglist.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "../components/Blog";
+import { beforeEach } from "vitest";
 
 describe("<Blog />", () => {
   const mockUser = { token: "testingToken", username: "user" };
@@ -13,14 +14,17 @@ describe("<Blog />", () => {
     user: { username: "usr", id: "userId" },
   };
 
-  const { container } = render(
-    <Blog
-      user={mockUser}
-      blog={mockBlog}
-      blogsArray={[mockBlog]}
-      setBlogsArray={vi.fn()}
-    />,
-  );
+  let container;
+  beforeEach(() => {
+    return ({ container } = render(
+      <Blog
+        user={mockUser}
+        blog={mockBlog}
+        blogsArray={[mockBlog]}
+        setBlogsArray={vi.fn()}
+      />,
+    ));
+  });
 
   test("blog's title and author are visible but url or likes are not visible by default", () => {
     const partialView = container.querySelector(".partialView");
@@ -28,5 +32,15 @@ describe("<Blog />", () => {
 
     expect(partialView).toBeInTheDocument();
     expect(fullView).not.toBeInTheDocument();
+  });
+
+  test("blog's URL and likes are shown when 'show' button is clicked.", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("show");
+    await user.click(button);
+
+    const fullView = container.querySelector(".fullView");
+
+    expect(fullView).toBeInTheDocument();
   });
 });
