f1e1b40cf6c4ec05f5b72baceb88f0d8bff82f63
hadi <hadishamsi854@gmail.com>
2026-05-19 03:48:11 +0500

    Exercise-5.31

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,38 +1,65 @@
+import { Box, TableContainer, Paper, Typography, Button } from "@mui/material";
 const Blog = ({ blog, user, handleLikeUpdate, handleDeleteBlog }) => {
-  // const [toggleFullView, setToggleFullView] = useState(false);
-
   if (!blog) return;
 
   const verifyForDeletion = () => {
     if (!user) return;
     if (user.username === blog.user.username) {
       return (
-        <button style={{ color: "red" }} onClick={handleDeleteBlog}>
+        <Button
+          variant="outlined"
+          color="error"
+          size="small"
+          onClick={handleDeleteBlog}
+        >
           delete
-        </button>
+        </Button>
       );
     }
   };
 
   return (
-    <>
-      <div style={{ margin: "8px", padding: "4px", border: "1px solid black" }}>
-        <div className="fullView">
-          {blog.title} by {blog.author}
-          <br />
+    <TableContainer component={Paper} style={{ marginTop: 10, padding: 10 }}>
+      <div className="fullView">
+        <Typography variant="h5">{blog.title}</Typography>
+
+        <Typography
+          variant="body1"
+          color="textDisabled"
+          sx={{ mb: 1 }}
+        >{`by ${blog.author}`}</Typography>
+
+        <Typography variant="body1" sx={{ mb: 1 }}>
           <a target="_blank" href={`https://${blog.url}`}>
             {blog.url}
           </a>
-          <br />
-          likes {blog.likes}
-          {user && <button onClick={handleLikeUpdate}>like</button>}
-          <br />
+        </Typography>
+
+        <Typography variant="body1" sx={{ mb: 1 }}>
           {`Added by ${blog.user.username}`}
-          <br />
+        </Typography>
+
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography
+            variant="body1"
+            sx={{ fontWeight: "bold", mr: 1 }}
+          >{`likes ${blog.likes}`}</Typography>
+
+          {user && (
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleLikeUpdate}
+              sx={{ mr: 1 }}
+            >
+              like
+            </Button>
+          )}
+
           {verifyForDeletion()}
-        </div>
+        </Box>
       </div>
-    </>
+    </TableContainer>
   );
 };
 
