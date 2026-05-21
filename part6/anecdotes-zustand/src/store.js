44754b7de50334e4329134e5205a2e45e0d49740
hadi <hadishamsi854@gmail.com>
2026-05-22 00:24:15 +0500

    Exercise-6.6

--- part6/anecdotes-zustand/src/store.js
+++ part6/anecdotes-zustand/src/store.js
@@ -19,6 +19,7 @@ const asObject = (anecdote) => ({
 
 const useAnecdoteStore = create((set) => ({
   anecdotes: anecdotesAtStart.map(asObject),
+  filter: "",
   actions: {
     vote: (id) =>
       set((state) => ({
@@ -30,9 +31,15 @@ const useAnecdoteStore = create((set) => ({
       })),
     add: (anecdote) =>
       set((state) => ({ anecdotes: state.anecdotes.concat(anecdote) })),
+    setFilter: (value) => set(() => ({ filter: value })),
   },
 }));
 
-export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes);
 export const useAnecdoteActions = () =>
   useAnecdoteStore((state) => state.actions);
+
+export const useAnecdotes = () => {
+  const anecdotes = useAnecdoteStore((state) => state.anecdotes);
+  const filter = useAnecdoteStore((state) => state.filter);
+  return anecdotes.filter((anecdote) => anecdote.content.includes(filter));
+};
